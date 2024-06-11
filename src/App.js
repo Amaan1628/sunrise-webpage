3259265968ca359f164d94806f6a084fe36d6e64
Amaan <118141092+Amaan1628@users.noreply.github.com>
2024-06-12 00:35:47 +0530

    half done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+  const API = 'https://api.sunrise-sunset.org/json'
+
+  const [lat,setLat] = useState('')
+  const [long,setLong] = useState('')
+
+  const handleLatitude = (event) => {
+    setLat(event.target.value);
+  }
+  const handleLongitude = (event) => {
+    setLong(event.target.value);
+  }
+
+  const getTime = async() => {
+    const res = await axios.get(`https://api.sunrise-sunset.org/json?lat=${lat}&lng=${long}`)
+    console.log(res);
+    console.log(res.data.results)
+    console.log(res.data.results.sunrise)
+    console.log(res.data.results.sunset)
+    
+    const sunrise = res.data.results.sunrise;
+    const sunset = res.data.results.sunset;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='flex flex-col'>
+      <h1>Sunset and Sunrise</h1>
+      <div>
+        <input onChange={handleLatitude} type="text">Whats the latitude? </input>
+        <input onChange={handleLongitude} type="text">Whats the longitude? </input>
+        <h2> {sunrise} </h2>
+        <h2> {sunset} </h2>
+      </div>
+      <button onClick={getTime}>GET Time</button>
     </div>
   );
 }
